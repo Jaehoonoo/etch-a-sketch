699d6b7e072ee3dbece177617b2c38bc55a30037
Jaehoonoo <justjhoon@gmail.com>
2024-03-04 11:26:45 -0500

    Created 16 squares to to DOM and established some css styles for alignment.

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",function() {
+    function createSquares() {
+        const grid = document.querySelector('.grid');
+
+        for (let i = 0; i <= 16; i++) {
+            const square = document.createElement('div');
+            square.className = 'square';
+            grid.appendChild(square);
+        }
+    }
+    createSquares();
+
+
+});
