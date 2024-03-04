54050323944410d48e1e33965de216ce2f6cdb68
Jaehoonoo <justjhoon@gmail.com>
2024-03-04 11:31:58 -0500

    Created viewport to device width in HTML and created a grid of square using CSS.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded",function() {
     function createSquares() {
         const grid = document.querySelector('.grid');
 
-        for (let i = 0; i <= 16; i++) {
+        for (let i = 0; i < 16; i++) {
             const square = document.createElement('div');
             square.className = 'square';
             grid.appendChild(square);
